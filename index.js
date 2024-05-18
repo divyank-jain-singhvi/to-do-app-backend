36b4d62c2518706546119687d94f980a6d0c233a
divyank-jain-singhvi <you@example.com>
2024-05-18 23:28:01 +0530

    added todo schema and nodemon already done

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-// const express=require('express');
-// const app=express();
-
-// const bodyParser=require('body-parser');
-
-// app.use(bodyParser.json())
-
-// app.listen(3000,()=>{
-//     console.log('server at port 3000')
-// })
-
-// app.get('/',(req,res)=>{
-//     res.send('kello world')
-// })
-
-// app.post('/api/cars',(request,response)=>{
-//     const {name1,brand}=request.body;
-//     console.log(name1)
-//     console.log(brand);
-//     response.send('cars');
-
-// })
-
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/mydatabase')
-// .then(() => { console.log('Connection successful') })
-// .catch((error) => { console.log('received an error') });
-
+const express=require("express");
+const app=express();
+app.listen(3000,()=>{
+    console.log("app is running");
+})
---
+++ models/Todo.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose");
+const todoschema=new mongoose.Schema({
+    Title:{
+        type:String,
+        require:true,
+        maxLength:50
+    },
+    description:{
+        type:String,
+        require:true,
+        maxLength:50
+    },
+    createdat:{
+        type:Date,
+        require:true,
+        default:Date.now()
+    },
+    updatedat:{
+        type:Date,
+        require:true,
+        default:Date.now()
+    }
+})
+
+model.exports=mongoose.model("Todo",todoschema);
