c210beceb1139758efd5b3cb8d163d6287d89834
divyank-jain-singhvi <you@example.com>
2024-05-20 22:15:02 +0530

    added todo delete operation and project is complete

---
+++ controllers/DeleteTodo.js
@@ -0,0 +1,21 @@
+const { response } = require("express");
+const Todo = require("../models/Todo");
+
+exports.DeleteTodo = async (req, res) => {
+    try {
+        const id =req.params.id;;
+        await Todo.findByIdAndDelete({_id:id});
+            res.json({
+                success:true,
+                message:"Todo Deleted successfully"
+            })
+        }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success:false,
+            data:"server error",
+            message:err.message
+        })
+    }
+};
--- routes/todos.js
+++ routes/todos.js
@@ -4,10 +4,12 @@ const router=express.Router();
 const {createTodo}=require("../controllers/CreateTodo");
 const {GetTodo,GetTodoID}=require("../controllers/GetTodo");
 const {UpdateTodo}=require("../controllers/UpdateTodo");
+const {DeleteTodo}=require("../controllers/DeleteTodo");
 
 router.post("/createTodo",createTodo);
 router.get("/getTodo",GetTodo);
 router.get("/getTodoid/:id",GetTodoID);
 router.put("/PutTodo/:id",UpdateTodo);
+router.delete("/deleteTodo/:id",DeleteTodo);
 
 module.exports=router;
