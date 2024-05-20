6e61a9411a49f805530d13d1f93e6663c5e2225c
divyank-jain-singhvi <you@example.com>
2024-05-20 19:12:19 +0530

    added fetching data by perticular id

--- controllers/GetTodo.js
+++ controllers/GetTodo.js
@@ -18,3 +18,29 @@ exports.GetTodo = async (req, res) => {
         })
     }
 };
+
+exports.GetTodoID = async (req, res) => {
+    try {
+        const id=req.params.id;  
+        const todo=await Todo.find({_id:id});
+        if (!todo){
+            return res.status(404).json({
+                success:false,
+                message:"data not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            data:todo,
+            message:`data ${id} successfully fetched`
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success:false,
+            data:"server error",
+            message:err.message
+        })
+    }
+};
+
--- routes/todos.js
+++ routes/todos.js
@@ -2,9 +2,10 @@ const express=require("express");
 const router=express.Router();
 
 const {createTodo}=require("../controllers/CreateTodo");
-const {GetTodo}=require("../controllers/GetTodo");
+const {GetTodo,GetTodoID}=require("../controllers/GetTodo");
 
 router.post("/createTodo",createTodo);
 router.get("/getTodo",GetTodo);
+router.get("/getTodoid/:id",GetTodoID);
 
 module.exports=router;
