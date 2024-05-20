b815381c5d5ba91b0b0bbba6a38063e0a8fa32b4
divyank-jain-singhvi <you@example.com>
2024-05-20 22:01:00 +0530

    added update data

--- routes/todos.js
+++ routes/todos.js
@@ -3,9 +3,11 @@ const router=express.Router();
 
 const {createTodo}=require("../controllers/CreateTodo");
 const {GetTodo,GetTodoID}=require("../controllers/GetTodo");
+const {UpdateTodo}=require("../controllers/UpdateTodo");
 
 router.post("/createTodo",createTodo);
 router.get("/getTodo",GetTodo);
 router.get("/getTodoid/:id",GetTodoID);
+router.put("/PutTodo/:id",UpdateTodo);
 
 module.exports=router;
